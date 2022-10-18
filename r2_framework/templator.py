8ff7f4af81e47742349a075ebed845f8aad7236a
Roman <slyusarrv@yandex.ru>
2022-10-18 11:15:53 +0500

    complete

--- r2_framework/templator.py
+++ r2_framework/templator.py
@@ -1,5 +1,5 @@
-from jinja2 import Template
-from os.path import join
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
@@ -9,9 +9,18 @@ def render(template_name, folder='templates', **kwargs):
     :param kwargs: keyword arguments for template
     :return:
     """
-    file_path = join(folder, template_name)
-    # Open template for name
-    with open(file_path, encoding='utf-8') as f:
-        template = Template(f.read())
-    # Render template with parameters
+    env = Environment()
+    # Specify folder for template finding
+    env.loader = FileSystemLoader(folder)
+    # Finding template
+    template = env.get_template(template_name)
     return template.render(**kwargs)
+
+    # --------------- Old code ------------
+    # file_path = join(folder, template_name)
+    # # Open template for name
+    # with open(file_path, encoding='utf-8') as f:
+    #     template = Template(f.read())
+    # # Render template with parameters
+    # return template.render(**kwargs)
+    # -------------------------------------
